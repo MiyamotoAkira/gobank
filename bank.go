abc28a61a8e645a9f83ceb152549ccaa820d3e85
Mattsi <mattsi.jansky@codurance.com>
2024-07-14 13:24:33 +0100

    Add and print a single deposit

--- bank.go
+++ bank.go
@@ -1,12 +1,38 @@
 package bank
 
+import (
+	"strconv"
+	"time"
+)
+
 type Account interface {
 	deposit(amount int)
 	withdraw(amount int)
 	printStatement() string
 }
 
-type StandardAccount struct {
+type Statement struct {
+	Date    string
+	Amount  int
+	Balance int
 }
 
-func (a StandardAccount) printStatement() string { return "Date       || Amount || Balance" }
+type StandardAccount struct {
+	statements []Statement
+}
+
+func (account *StandardAccount) printStatement() string {
+	result := "Date       || Amount || Balance"
+
+	for _, statement := range account.statements {
+		result += "\n" + statement.Date + " || " + strconv.Itoa(statement.Amount) + "   || " + strconv.Itoa(statement.Balance)
+	}
+
+	return result
+}
+
+func (account *StandardAccount) deposit(amount int) {
+	date := time.Now().Format("2006-01-02")
+
+	account.statements = append(account.statements, Statement{date, amount, amount})
+}
--- bank_test.go
+++ bank_test.go
@@ -6,8 +6,19 @@ import (
 )
 
 func Test_EmptyPrint(t *testing.T) {
-	account := StandardAccount{}
+	account := new(StandardAccount)
 	actual := account.printStatement()
 	expected := "Date       || Amount || Balance"
 	assert.Equal(t, expected, actual, "The headers are not the same")
+}
+
+func Test_SingleDeposit(t *testing.T) {
+	account := new(StandardAccount)
+
+	account.deposit(100)
+	actual := account.printStatement()
+	expected := `Date       || Amount || Balance
+2024-07-14 || 100   || 100`
+
+	assert.Equal(t, expected, actual, "Deposit should be there")
 }

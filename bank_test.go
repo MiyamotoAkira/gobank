c246c797f869365d0420b180d0aa17583958958a
Mattsi <mattsi.jansky@codurance.com>
2024-07-14 14:39:15 +0100

    Show correct days date on day transaction is made

--- bank_test.go
+++ bank_test.go
@@ -55,3 +55,34 @@ func Test_DepositThenWithdrawal(t *testing.T) {
 
 	assert.Equal(t, expected, actual, "Deposit should be there")
 }
+
+func Test_DepositsOnDifferentDates(t *testing.T) {
+	i := 0
+	mockedDateProvider := func() Date {
+		result := Date("2012-01-10")
+		if i == 1 {
+			result = "2012-01-13"
+		} else if i == 2 {
+			result = "2012-01-14"
+		}
+
+		i++
+		return result
+	}
+	account := StandardAccount{TodaysDateProvider: mockedDateProvider}
+
+	//Mock date to 2012-01-10
+	account.deposit(1000)
+	//Mock date to 2012-01-13
+	account.deposit(2000)
+	//Mock date to 2012-01-14
+	account.withdraw(500)
+
+	expected := "" +
+		"Date       || Amount || Balance\n" +
+		"2012-01-14 || -500    || 2500\n" +
+		"2012-01-13 || 2000    || 3000\n" +
+		"2012-01-10 || 1000    || 1000"
+	actual := account.printStatement()
+	assert.Equal(t, expected, actual, "Expected statement should match")
+}
